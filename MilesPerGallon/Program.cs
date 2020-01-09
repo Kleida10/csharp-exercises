db977ee6bb512eb01fbbbc95b7084088b19ed9a0
Kleida10 <56648839+Kleida10@users.noreply.github.com>
2020-01-08 18:21:16 -0600

    done

---
+++ MilesPerGallon/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MilesPerGallon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double miles;
+            double gallons;
+            string input;
+
+            Console.WriteLine("How many miles did you drive?: ");
+            input = Console.ReadLine();
+            miles = double.Parse(input);
+
+            Console.WriteLine("How many gallons of gas did you consume?: ");
+            input = Console.ReadLine();
+            gallons = double.Parse(input);
+
+            double milesPerGallon = miles / gallons;
+
+            Console.WriteLine("You did: " + milesPerGallon + " miles per gallon");
+
+        }
+    }
+}
---
+++ RectangleArea/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sideA;
+            int sideB;
+            string input1;
+            string input2;
+           
+            Console.WriteLine("What are the dimensions of the rectangle?: ");
+           
+            input1 = Console.ReadLine();
+            sideA = int.Parse(input1);
+            
+            input2 = Console.ReadLine();
+            sideB = int.Parse(input2);
+            
+            int area = sideA * sideB;
+            
+            Console.WriteLine("The area of the rectangle is: " + area);
+        }
+    }
+}
