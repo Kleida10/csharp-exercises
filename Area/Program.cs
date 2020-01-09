144cda276fd7db7873914c0ae5c3d1661a1aa668
Kleida10 <56648839+Kleida10@users.noreply.github.com>
2020-01-08 20:11:13 -0600

    done

---
+++ Area/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                double radius;
+                string input;
+
+                Console.WriteLine("What is the radius?: ");
+                input = Console.ReadLine();
+
+                radius = double.Parse(input);
+                if (radius < 0)
+                {
+                    Console.WriteLine("Incorrect number. Please input a positive number.");
+                }
+                else
+                {
+                    double area = 3.14 * radius * radius;
+                    Console.WriteLine("The area of a circle with radius " + radius + " is " + area);
+                    break;
+                }
+
+            }
+            
+
+               
+        }
+    }
+}
